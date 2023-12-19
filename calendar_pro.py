4bbdc650bf2a7bba297cf80ca520e0675f7f92d6
PratyayPrasoon19 <154357898+PratyayPrasoon19@users.noreply.github.com>
2023-12-19 22:16:56 +0530

    Update calendar_pro.py

--- calendar_pro.py
+++ calendar_pro.py
@@ -39,23 +39,3 @@ def printmonth(mm,yyyy):
 def printyear(yyyy):
     for i in range (1,13):
         printmonth(i,yyyy)
-def day(dd,mm,yyyy):
-    lpyr=0
-    for i in range(1900,yyyy):
-        if (i%4==0 and i%100!=0 or i%400==0):
-            lpyr+=1
-    no_of_days = (yyyy-1900)*365+lpyr+dd
-    for i in range(1,mm):
-        if i==2:
-            if (yyyy%4==0 and yyyy%100!=0 or yyyy%400==0):
-                no_of_days+=1
-            else :
-                no_of_days+=0
-        elif i in [1,3,5,7,8,10,12]:
-            no_of_days+=31
-        elif i in [4,6,9,11]:
-            no_of_days+=30
-    
-    weekday=no_of_days%7
-    day={0:'Monday',1:'Tuesday',2:'Wednesday',3:'Thursday',4:'Friday',5:'Saturday',6:'Sunday'}
-    return day[weekday]
